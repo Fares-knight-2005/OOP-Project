0bf0d8c49d05dd56b37f364749a74e1894e8b20e
Fares <fares.oyion123@gmail.com>
2024-12-25 12:53:10 +0300

    After Change

---
+++ TransactionScreen.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include "Screen.h"
+#include <iomanip>
+#include "MainMenuScreen.h"
+#include "DeposetScreen.h"
+#include "clsWithdrawScreen.h"
+#include "clsTotalBalancesScreen.h"
+#include "clsTransferScreen.h"
+#include "clsTransferRegesterScreen.h"
+
+using namespace std;
